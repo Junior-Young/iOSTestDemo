2e2d875ce89a3d7763f2f37d8359367435078af8
Junior-Young <327762208@qq.com>
2018-07-26 16:16:12 +0800

    feat: Model类

---
+++ newDemo/newDemo/YModel.h
@@ -0,0 +1,15 @@
+//
+//  YModel.h
+//  newDemo
+//
+//  Created by 杨俊杰 on 2018/7/26.
+//  Copyright © 2018年 杨俊杰. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YModel : NSObject
+
+- (NSString *)name;
+
+@end
---
+++ newDemo/newDemo/YModel.m
@@ -0,0 +1,19 @@
+//
+//  YModel.m
+//  newDemo
+//
+//  Created by 杨俊杰 on 2018/7/26.
+//  Copyright © 2018年 杨俊杰. All rights reserved.
+//
+
+#import "YModel.h"
+
+@implementation YModel
+
+
+- (NSString *)name {
+    NSString *name = @"yang";
+    return name
+}
+
+@end
